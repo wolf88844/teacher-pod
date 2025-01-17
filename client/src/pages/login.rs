f5c5f5083b53ee41aff3be0e69cbc7a9da3af503
wolf88844 <wolf88840@163.com>
2025-01-17 18:27:52 +0800

    feat(client): 添加登录页面组件
    - 实现了一个基本的登录页面组件，包含标题和样式
    - 使用 Dioxus 框架构建响应式用户界面
    - 集成了全局状态管理，支持显示提示信息

--- client/src/pages/login.rs
+++ client/src/pages/login.rs
@@ -1,1 +1,24 @@
 
+use dioxus::prelude::*;
+
+use crate::TOAST;
+
+pub fn Login()->Element{
+    let toast = TOAST.signal();
+
+    let email = use_signal(||String::new);
+    let password = use_signal(||String::new);
+
+    rsx!(
+        div{
+            class:"justify-center",
+            div{
+                class:"p-10 xs:p-0 mx-auto md:w-full md:max-w-md",
+                h1{
+                    class:"font-bold text-center text-black dark:text-white text-2x mb-5",
+                    "Sign In | TeacherPod"
+                }
+            }
+        }
+    )
+}
